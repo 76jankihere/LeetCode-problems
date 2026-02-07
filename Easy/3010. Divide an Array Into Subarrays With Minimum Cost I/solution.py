25d98e322da59465c5dc5604552c50f6a3a923af
Janki P. Parmar <108918275+76jankihere@users.noreply.github.com>
2026-02-07 17:41:57 -0600

    Add minimumCost method to Solution class
    Implement minimumCost method to calculate minimum cost of dividing an array into subarrays.

---
+++ Easy/3010. Divide an Array Into Subarrays With Minimum Cost I/solution.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def minimumCost(self, nums):
+        m1 = 10**9
+        m2 = 10**9
+        for x in nums[1:]:
+            if x < m1:
+                m2 = m1
+                m1 = x
+            elif x < m2:
+                m2 = x
+        return nums[0] + m1 + m2
